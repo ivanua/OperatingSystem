e69253cf12c58ece2ccbecd0a0d41755bfbbd21c
Ivan-Khomin <homin151210@gmail.com>
2025-11-30 10:02:31 +0000

    updated

--- src/bootloader/stage2/stdio.c
+++ src/bootloader/stage2/stdio.c
@@ -12,28 +12,23 @@ static const uint8_t DEFAULT_COLOR = 0x7;
 static uint8_t* screen_buffer = (uint8_t*)0xB8000;
 static int screen_x = 0, screen_y = 0;
 
-void putchr(int x, int y, char c)
-{
+void putchr(int x, int y, char c) {
     screen_buffer[2 * (y * SCREEN_WIDTH + x)] = c;
 }
 
-void putcolor(int x, int y, uint8_t color)
-{
+void putcolor(int x, int y, uint8_t color) {
     screen_buffer[2 * (y * SCREEN_WIDTH + x) + 1] = color;
 }
 
-char getchr(int x, int y)
-{
+char getchr(int x, int y) {
     return screen_buffer[2 * (y * SCREEN_WIDTH + x)];
 }
 
-uint8_t getcolor(int x, int y)
-{
+uint8_t getcolor(int x, int y) {
     return screen_buffer[2 * (y * SCREEN_WIDTH + x) + 1];
 }
 
-void setcursor(int x, int y)
-{
+void setcursor(int x, int y) {
     int pos = y * SCREEN_WIDTH + x;
 
     x86_outb(0x3D4, 0x0F);
@@ -42,11 +37,9 @@ void setcursor(int x, int y)
     x86_outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
 }
 
-void clrscr()
-{
+void clrscr() {
     for (int y = 0; y < SCREEN_HEIGHT; y++)
-        for (int x = 0; x < SCREEN_WIDTH; x++)
-        {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
             putchr(x, y, '\0');
             putcolor(x, y, DEFAULT_COLOR);
         }
@@ -56,18 +49,15 @@ void clrscr()
     setcursor(screen_x, screen_y);
 }
 
-void scrollback(int lines)
-{
+void scrollback(int lines) {
     for (int y = lines; y < SCREEN_HEIGHT; y++)
-        for (int x = 0; x < SCREEN_WIDTH; x++)
-        {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
             putchr(x, y - lines, getchr(x, y));
             putcolor(x, y - lines, getcolor(x, y));
         }
 
     for (int y = SCREEN_HEIGHT - lines; y < SCREEN_HEIGHT; y++)
-        for (int x = 0; x < SCREEN_WIDTH; x++)
-        {
+        for (int x = 0; x < SCREEN_WIDTH; x++) {
             putchr(x, y, '\0');
             putcolor(x, y, DEFAULT_COLOR);
         }
@@ -75,42 +65,36 @@ void scrollback(int lines)
     screen_y -= lines;
 }
 
-void putc(char c)
-{
-    switch (c)
-    {
-        case '\n':
-            screen_x = 0;
-            screen_y++;
-            break;
-        case '\t':
-            for (int i = 0; i < 4 - (screen_x % 4); i++)
-                putc(' ');
-            break;
-        case '\r':
-            screen_x = 0;
-            break;
-        default:
-            putchr(screen_x, screen_y, c);
-            screen_x++;
-            break;
+void putc(char c) {
+    switch (c) {
+    case '\n':
+        screen_x = 0;
+        screen_y++;
+        break;
+    case '\t':
+        for (int i = 0; i < 4 - (screen_x % 4); i++)
+            putc(' ');
+        break;
+    case '\r':
+        screen_x = 0;
+        break;
+    default:
+        putchr(screen_x, screen_y, c);
+        screen_x++;
+        break;
     }
 
-    if (screen_x >= SCREEN_WIDTH)
-    {
+    if (screen_x >= SCREEN_WIDTH){
         screen_y++;
         screen_x = 0;
-    }
-    if (screen_y >= SCREEN_HEIGHT)
+    } if (screen_y >= SCREEN_HEIGHT)
         scrollback(1);
 
     setcursor(screen_x, screen_y);
 }
 
-void puts(const char* str)
-{
-    while(*str)
-    {
+void puts(const char* str) {
+    while(*str) {
         putc(*str);
         str++;
     }
@@ -118,14 +102,12 @@ void puts(const char* str)
 
 const char hexchars[] = "0123456789abcdef";
 
-void printf_unsigned(unsigned long long number, int radix)
-{
+void printf_unsigned(unsigned long long number, int radix) {
     char buffer[32];
     int pos = 0;
 
     // convert number to ASCII
-    do 
-    {
+    do {
         unsigned long long rem = number % radix;
         number /= radix;
         buffer[pos++] = hexchars[rem];
@@ -136,10 +118,8 @@ void printf_unsigned(unsigned long long number, int radix)
         putc(buffer[pos]);
 }
 
-void printf_signed(long long number, int radix)
-{
-    if (number < 0)
-    {
+void printf_signed(long long number, int radix) {
+    if (number < 0) {
         putc('-');
         printf_unsigned(-number, radix);
     }
@@ -158,8 +138,7 @@ void printf_signed(long long number, int radix)
 #define PRINTF_LENGTH_LONG          3
 #define PRINTF_LENGTH_LONG_LONG     4
 
-void printf(const char* fmt, ...)
-{
+void printf(const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
 
@@ -169,13 +148,10 @@ void printf(const char* fmt, ...)
     bool sign = false;
     bool number = false;
 
-    while (*fmt)
-    {
-        switch (state)
-        {
+    while (*fmt) {
+        switch (state) {
         case PRINTF_STATE_NORMAL:
-            switch (*fmt)
-            {
+            switch (*fmt) {
             case '%':
                 state = PRINTF_STATE_LENGTH;
                 break;
@@ -184,8 +160,7 @@ void printf(const char* fmt, ...)
             }
             break;
         case PRINTF_STATE_LENGTH:
-            switch (*fmt)
-            {
+            switch (*fmt) {
             case 'h':
                 length = PRINTF_LENGTH_SHORT;
                 state = PRINTF_STATE_LENGTH_SHORT;
@@ -198,16 +173,14 @@ void printf(const char* fmt, ...)
             }
             break;
         case PRINTF_STATE_LENGTH_SHORT:
-            if (*fmt == 'h')
-            {
+            if (*fmt == 'h') {
                 length = PRINTF_LENGTH_SHORT_SHORT;
                 state = PRINTF_STATE_SPEC;
             }
             else goto PRINTF_STATE_SPEC_;
             break;
         case PRINTF_STATE_LENGTH_LONG:
-            if (*fmt == 'l')
-            {
+            if (*fmt == 'l') {
                 length = PRINTF_LENGTH_LONG_LONG;
                 state = PRINTF_STATE_SPEC;
             }
@@ -215,8 +188,7 @@ void printf(const char* fmt, ...)
             break;
         case PRINTF_STATE_SPEC:
         PRINTF_STATE_SPEC_:
-            switch (*fmt)
-            {
+            switch (*fmt) {
             case 'c':
                 putc((char)va_arg(args, int));
                 break;
@@ -251,12 +223,9 @@ void printf(const char* fmt, ...)
                 break;
             }
 
-            if (number)
-            {
-                if (sign)
-                {
-                    switch (length)
-                    {
+            if (number) {
+                if (sign) {
+                    switch (length) {
                     case PRINTF_LENGTH_SHORT_SHORT:
                     case PRINTF_LENGTH_SHORT:
                     case PRINTF_LENGTH_DEFAULT:
@@ -269,11 +238,8 @@ void printf(const char* fmt, ...)
                         printf_signed(va_arg(args, long long), radix);
                         break;
                     }
-                }
-                else
-                {
-                    switch (length)
-                    {
+                } else {
+                    switch (length) {
                     case PRINTF_LENGTH_SHORT_SHORT:
                     case PRINTF_LENGTH_SHORT:
                     case PRINTF_LENGTH_DEFAULT:
